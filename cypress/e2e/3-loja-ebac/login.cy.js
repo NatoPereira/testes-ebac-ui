1a0f9b970c9aec96e661ea3d214b37c6f59632f1
Nato Castro <natocastro@outlook.com>
2025-10-14 20:11:18 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,12 +2,33 @@
 
 describe('Funcionalidade: Login', () => {
 
-    it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () =>{
         cy.get('#username').type('natocastro@outlook.com')
         cy.get('#password').type('Natocastro1,')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, natocastro (não é natocastro? Sair)')
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('natocastr@outlook.com')
+        cy.get('#password').type('Natocastro1,')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('exist')
+    });
+    
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('natocastro@outlook.com')
+        cy.get('#password').type('Natocastro1')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain' , 'A senha fornecida para o e-mail natocastro@outlook.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error > li').should('exist')
+    });
 })
