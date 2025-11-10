2d5c43f0548722577b02c01b15ee1ad547c11561
Nato Castro <natocastro@outlook.com>
2025-11-10 11:46:17 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,21 +1,45 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-       cy.get('.products > .row')
-       //.first()
-       //.last()
-       //.eq(2)
-       .contains('Ajax Full-Zip Sweatshirt')
-       .click()
-       
+       produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
        cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
            
-           
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Ajax Full-Zip Sweatshirt'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto )
+    });
+    
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
+    });
+
+    it('Deve adicinar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Green', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicinar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados =>{
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+               dados[2].tamanho, 
+               dados[2].cor, 
+               dados[2].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)   
+        })
+        
+        
+    });
 });
